5126f5ba77c22d740c4c84e9019cd19c1e458420
JulietteCoemelck <92322714+JulietteCoemelck@users.noreply.github.com>
2021-12-24 12:50:34 +0100

    Delete villesDetA.js

--- reducers/villesDetA.js
+++
@@ -1,9 +0,0 @@
-export default function(villesDetA = [], action){
-    if(action.type == 'villesDetA'){
-        console.log('reducer villes D et A',action.villesDetA)
-        return action.villesDetA
-
-    } else {
-        return villesDetA
-    }
-}
